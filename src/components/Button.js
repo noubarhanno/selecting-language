bbd3d0e4bf2afddc857e101f231bf7f2a4d07681
Noubar Jack Hanno <noubar@Noubars-MacBook-Pro-2.local>
2019-05-16 08:20:49 +0400

    we created the context object and assign it to button component and we console log it

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import LanguageContext from '../contexts/LanguageContext';
 
 class Button extends React.Component{
+    static contextType = LanguageContext;
+
     render(){
+        console.log(this.context);
         return (
             <button className="ui button primary">Submit</button>
         );
     }
 }
+
+// alternatively we can write the context type as per the below line instead of defining the context type inside the Button class
+// Button.contextType = LanguageContext;
 
 export default Button;
