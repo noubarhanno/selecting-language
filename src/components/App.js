ab92e5dcf8cd0228189a76d116bf517790036c2b
Noubar Jack Hanno <noubar@Noubars-MacBook-Pro-2.local>
2019-05-16 10:26:20 +0400

    We passed two context to button look into Button Class to see how to pull the values out

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,37 @@
 import React from "react";
-import UserCreat from './UserCreate';
-import LanguageContext from '../contexts/LanguageContext';
+import UserCreat from "./UserCreate";
+import LanguageContext from "../contexts/LanguageContext";
+import ColorContext from "../contexts/ColorContext";
 
 class App extends React.Component {
-    state = { language: 'english' };
+  state = { language: "english" };
 
-    onLanguageChange = language => {
-        this.setState({language});
-    }
+  onLanguageChange = language => {
+    this.setState({ language });
+  };
 
   render() {
     return (
-     <div className="ui container">
+      <div className="ui container">
         <div>
-            Select a language:
-            <i className="flag us" onClick={() => this.onLanguageChange('english')} />
-            <i className="flag nl" onClick={() => this.onLanguageChange('dutch')} />
+          Select a language:
+          <i
+            className="flag us"
+            onClick={() => this.onLanguageChange("english")}
+          />
+          <i
+            className="flag nl"
+            onClick={() => this.onLanguageChange("dutch")}
+          />
         </div>
-        <LanguageContext.Provider value={this.state.language}>
+        {/* doent matter if we wrap the colorContext first then LanguageContext or the LanguageContext first then the Color Context
+        this example of passing multiple context values to the nested child component */}
+        <ColorContext.Provider value="red">
+          <LanguageContext.Provider value={this.state.language}>
             <UserCreat />
-        </LanguageContext.Provider>
-     </div>
+          </LanguageContext.Provider>
+        </ColorContext.Provider>
+      </div>
     );
   }
 }
